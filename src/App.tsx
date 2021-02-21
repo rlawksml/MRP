54ad11f3c1236a2609eec570227be1a155c58605
rlawksml <rlawksml@gmail.com>
2021-02-21 23:45:55 +0900

    web grid design

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import ResetCss from './style/reset';
+
+const App = () => {
+    return(
+    <>
+
+
+    </>);
+
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Grid, Typography, Button } from '@material-ui/core';
+import styled from 'styled-components';
+import 'fontsource-roboto';
+
+import { randomInt } from 'mathjs';
+import { Redirect } from 'react-router';
+import { isBrowser } from 'react-device-detect';
