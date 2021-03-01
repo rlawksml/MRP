118dc93856fa1922bc6738137924e15ffbb3c8fc
JINHEE KIM <rlawksml@gmail.com>
2021-03-02 00:24:27 +0900

    Delete client.jsx

--- client.jsx
+++
@@ -1,6 +0,0 @@
-const { Component } = require('react');
-const React = require('react');
-const ReactDOM = require('react-dom');
-const Home = require('./src/Home.jsx');
-
-ReactDOM.render(<Home />, document.querySelector('#root'));
