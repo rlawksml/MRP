cff2472bae79042c614b41a393541c180ba353f4
JINHEE KIM <rlawksml@gmail.com>
2021-04-19 18:27:06 +0900

    contact page / Cart page renew

--- src/coponents/CartComponent.jsx
+++ src/coponents/CartComponent.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Grid, container } from '@material-ui/core';
-import ReactStars from "react-rating-stars-component"
+import ReactStars from 'react-rating-stars-component';
 
 export default () => {
-  const ratingChanged = (newRating)=>{
-    console.log(newRating)
-  }
-  
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+  const onDelete = (e) => {};
   return (
     <>
-          <CartGrid>
-            {/* 장바구니 그리드 */}
-            <Deletebutton>X</Deletebutton>
-            {/* 삭제 버튼 */}
-            <ItemBtm>
-              {/* 별점 버튼 */}
-              <ReactStars
-              precision={0.5}
-              defaultValue={2.5}
-                count={5} size={24}
-              activeColor="#ffd700"
-              onChange={ratingChanged}
-              ></ReactStars>
-              {/* react 라이브러리 react rating stars component 사용해서 별점 입력 onchange로 위에 설정해 둔 함수 raringChagned에 값 전달함 */}
-            </ItemBtm>
-          </CartGrid>
+      <CartGrid>
+        {/* 장바구니 그리드 */}
+        <Deletebutton onChange={onDelete}>X</Deletebutton>
+        {/* 삭제 버튼 */}
+        <ItemBtm>
+          {/* 별점 버튼 */}
+          <ReactStars
+            precision={0.5}
+            defaultValue={2.5}
+            count={5}
+            size={24}
+            activeColor="#ffd700"
+            onChange={ratingChanged}
+          ></ReactStars>
+          {/* react 라이브러리 react rating stars component 사용해서 별점 입력 onchange로 위에 설정해 둔 함수 raringChagned에 값 전달함 */}
+        </ItemBtm>
+      </CartGrid>
     </>
   );
 };
-
 
 const CartGrid = styled(Grid)`
   position: relative;
@@ -38,7 +38,7 @@ const CartGrid = styled(Grid)`
   margin: 2rem 10px;
   box-shadow: 5px 5px 10px 3px #999999;
   border-radius: 10px 10px 0 0;
-  `;
+`;
 
 const ItemBtm = styled.div`
   position: absolute;
@@ -47,29 +47,29 @@ const ItemBtm = styled.div`
   height: 50px;
   background-color: #3956a5;
   opacity: 0.85;
-  display:flex;
-  justify-content:center;
-  align-items:center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   top: 300px;
   transition: 0.3s;
   box-shadow: 5px 5px 10px 3px #999999;
-  border-radius: 0 0 10px 10px ;
+  border-radius: 0 0 10px 10px;
 `;
 
 const Deletebutton = styled.button`
   position: abouslute;
-  transform : translate(220px, 8px);
+  transform: translate(220px, 8px);
   border-radius: 100%;
-  display:flex;
-  align-items : center;
+  display: flex;
+  align-items: center;
   justify-content: center;
   font-size: 1rem;
   width: 1.5rem;
   height: 1.5rem;
   color: #f4f4f4;
   background-color: #7292cc;
-  border:0;
-  outline:none;
+  border: 0;
+  outline: none;
   &:hover {
     background: #254489;
     font-weight: bold;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,154 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Grid, container } from '@material-ui/core';
-import Navigation from '../coponents/Navigation';
 
 export default () => {
+  const Sumbit = () => {
+    console.log('hello');
+  };
   return (
     <>
-      {/* <h1>Contact(개발자) 페이지</h1> */}
-      {/* <Navigation /> */}
-      <MainContainer>
-        <SectionContain
-          item
-          xs={12}
-          sm={6}
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-          <DevDiv1>
-            <h3>개발자</h3>
-          </DevDiv1>
-          <DevDiv2>
-            <h3>개발자</h3>
-          </DevDiv2>
-        </SectionContain>
-
-        <SectionContain
-          item
-          xs={12}
-          sm={6}
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-          <InfoContainer>
-            <NameDiv>이름</NameDiv>
-            <PhoneDiv>연락처</PhoneDiv>
-            <EmailDiv>이메일</EmailDiv>
-          </InfoContainer>
-          <TextContainer>
-            <TextDiv>내용</TextDiv>
-            <SubmitDiv>보내기</SubmitDiv>
-          </TextContainer>
-        </SectionContain>
-      </MainContainer>
+      <Form action="">
+        <InfoContainer>
+          <label htmlFor="name">Name : </label>
+          <input type="text"></input>
+        </InfoContainer>
+        <InfoContainer>
+          <label htmlFor="mail">E-mail : </label>
+          <input type="email"></input>
+        </InfoContainer>
+        <InfoContainer>
+          <label htmlFor="message">Message : </label>
+          <textarea name="message" id="msg" cols="30" rows="10"></textarea>
+        </InfoContainer>
+        <InfoContainer>
+          {/* <Submit type="submit" onClick={Submit}>
+            보내기
+          </Submit> */}
+          <button type="submit" onClick={Submit}></button>
+        </InfoContainer>
+      </Form>
+      <Footer>
+        <a href="https://github.com/rlawksml/MRP.git">
+          <p>Github : https://github.com/rlawksml/MRP.git</p>
+        </a>
+      </Footer>
     </>
   );
 };
 
-const MainContainer = styled.div`
-  color: red;
-  border: 1px solid blue;
-  height: 50em;
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const DevDiv1 = styled.div`
-
-  box-shadow: 2px 2px 10px 2px #7292cc;
-  border-radius: 100%;
-  height: 200px;
-  width: 200px;
-  margin-right: 50px;
-  transition: 0.3s;
-  &:hover {
-    background-color: #c4c5c6;
-  }
-`;
-const DevDiv2 = styled.div`
-
-  box-shadow: 2px 2px 10px 2px #7292cc;
-  border-radius: 100%;
-  height: 200px;
-  width: 200px;
-  margin-right: 15px;
-  transition: 0.3s;
-  &:hover {
-    background-color: #c4c5c6;
-  }
-`;
-
-const SectionContain = styled(Grid)`
-
-  border: 1px solid red;
-  height: 40em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
-
 const InfoContainer = styled.div`
-  position: absolute;
-  top: 100px;
-  border: 1px dashed black;
-  width: 80%;
-  height: 80px;
+  // border: 1px dashed black;
+  margin: 10px;
+  width: 300px;
+`;
+
+const Submit = styled.button`
+  position: relative;
+  top: 0px;
+  right: -180px;
+`;
+
+const Form = styled.form`
+  border: 1px solid red;
+  width: 100%;
+  height: 80vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
-`;
-const TextContainer = styled.div`
-  position: absolute;
-  top: 200px;
-  border: 1px dashed black;
-  width: 80%;
-  height: 250px;
-  display: flex;
+  justify-content: center;
   flex-direction: column;
+  font-family: roboto;
 `;
 
-const NameDiv = styled.div`
-  border: 1px solid blue;
+const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 0;
   width: 100%;
-  height: 25px;
-  padding: 5px 0;
-`;
-
-const PhoneDiv = styled.div`
-  border: 1px solid blue;
-  width: 100%;
-  height: 25px;
-  padding: 5px 0;
-`;
-const EmailDiv = styled.div`
-  border: 1px solid blue;
-  width: 100%;
-  height: 25px;
-  padding: 5px 0;
-`;
-
-const TextDiv = styled.div`
-  border: 1px solid red;
-  width: 100%;
-  height: 200px;
-  padding: 5px;
-`;
-
-const SubmitDiv = styled.div`
-  position: absolute;
-  top: 250px;
-  right: 0px;
-  border: 1px solid red;
-  width: 70px;
-  height: 50px;
-  padding: 5px;
+  height: 70px;
+  font-size: 1rem;
+  font-weight: 400;
+  background-color: #7292cc;
+  color: white;
+  &:hover {
+    background-color: #4e6caf;
+  }
 `;
